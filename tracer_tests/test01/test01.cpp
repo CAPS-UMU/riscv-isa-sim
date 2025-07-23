b890c6f9023a907930009546ef62c82fec7b4a1b
Ricardo Fernández Pascual <ricardof@um.es>
2025-07-23 12:16:26 +0200

    Fix vectorization of test01.

--- tracer_tests/test01/test01.cpp
+++ tracer_tests/test01/test01.cpp
@@ -1,23 +1,21 @@
 #include "g4tracer-interface.h"
 
-const int n = 100000;
+const uint64_t n = 1000000;
 double a[n], b[n], c[n];
 double k = 7.23;
 
 int main() {
   g4tracer_init_current_thread();
   g4tracer_start_tracing();
-  for(int i = 0; i < n; i++) {
-    a[i] = 0.125;
-    b[i] = 0.25;
+  for(uint64_t i = 0; i < n; i++) {
+    a[i] = 0.125 + i;
+    b[i] = 0.25 + i;
     c[i] = 0.5;
   }
 
   g4tracer_start_ROI_verbose();
-  for (int j = 0; j < 10; ++j) {
-    for(int i = 0; i < n; i++) {
-      c[i] = c[i] * j + a[i] + b[i] * k;
-    }
+  for(uint64_t i = 0; i < n; i++) {
+    c[i] = a[i] + b[i] * k;
   }
   g4tracer_end_ROI_verbose();
 
