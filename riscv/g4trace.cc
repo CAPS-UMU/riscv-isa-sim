239b55dd62c7062b8dde100c467f5c67558b300e
Ricardo Fernández Pascual <ricardof@um.es>
2025-08-26 19:29:08 +0200

    Add Vector Strided stores.

--- riscv/g4trace.cc
+++ riscv/g4trace.cc
@@ -233,6 +233,7 @@ static G4TraceDecoder g4trace_get_decoder_internal(const string& instr_name) { /
   } else if (eq_any(instr_name,
                     "sb", "sd", "sh", "sw",
                     "vse8_v", "vse16_v", "vse32_v", "vse64_v",
+                    "vsse8_v", "vsse16_v", "vsse32_v", "vsse64_v",
                     "vsuxei8_v", "vsuxei16_v", "vsuxei32_v", "vsuxei64_v",
                     "vsm_v")) {
     return [](DECODER_ARGS) {
